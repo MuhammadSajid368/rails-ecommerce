987fea7c354d082dfd6487a189057b0e128d9bd9
MuhammadSajid368 <muhammadsajid936303@gmail.com>
2024-08-22 18:40:47 +0500

    Describe what this commit changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_cart
+  # before_action :set_cart
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :authenticate_user!, if: :devise_controller?
@@ -8,17 +8,17 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def set_cart
-    if session[:cart_id]
-      cart = Cart.find_by(id: session[:cart_id])
-      cart.present? ? (@current_cart = cart) : (session[:cart_id] = nil)
-    end
+  # def set_cart
+  #   if session[:cart_id]
+  #     cart = Cart.find_by(id: session[:cart_id])
+  #     cart.present? ? (@current_cart = cart) : (session[:cart_id] = nil)
+  #   end
 
-    if session[:cart_id].nil?
-      @current_cart = Cart.create
-      session[:cart_id] = @current_cart.id
-    end
-  end
+  #   if session[:cart_id].nil?
+  #     @current_cart = Cart.create
+  #     session[:cart_id] = @current_cart.id
+  #   end
+  # end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
